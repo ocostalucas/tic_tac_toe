7aeb328183d594dd90306f98ae0d6d6ba7a169b2
Lucas Costa <olucascosta98@gmail.com>
2021-08-14 11:25:43 -0300

    feat: Adiciona login screen

---
+++ lib/app/modules/auth/login/login_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/app/shared/components/buttons/social_button_widget.dart';
+import 'package:tic_tac_toe/app/shared/core/app_colors.dart';
+import 'package:tic_tac_toe/app/shared/core/app_images.dart';
+import 'package:tic_tac_toe/app/shared/core/app_typography.dart';
+
+class LoginScreen extends StatelessWidget {
+  LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Jogo da Velha', style: AppTypography.heading),
+            Container(
+              height: 4,
+              width: 150,
+              color: AppColors.white,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 50),
+              child: Image.asset(
+                AppImages.boardLogo,
+                width: MediaQuery.of(context).size.width * .55,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: SocialButtonWidget(
+                onTap: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
